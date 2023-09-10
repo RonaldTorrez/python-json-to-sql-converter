a77bf6123103518655596f66ced26134d02e6b2e
Ronald Torrez <dev@ronaldtorrez.com>
2023-09-10 17:48:17 -0400

    Now using TRY EXCEPTION

--- data/loaddata.py
+++ data/loaddata.py
@@ -1,15 +1,30 @@
-import os
-
-from dotenv import load_dotenv
-
 from utils.getdata import get_json_data
 from utils.path import data_dir
 
-load_dotenv()
-
 
 def load_country_data():
-    return get_json_data(os.getenv(
-        "COUNTRY_DATA_JSON"),
-        data_dir()
-    )
+	return get_json_data(
+		"countries_data",
+		data_dir()
+	)
+
+
+def load_country_official_lang_data():
+	return get_json_data(
+		"countries_official_language",
+		data_dir()
+	)
+
+
+def load_country_other_lang_data():
+	return get_json_data(
+		"countries_other_languages",
+		data_dir()
+	)
+
+
+def load_languages_data():
+	return get_json_data(
+		"languages",
+		data_dir()
+	)
--- utils/getdata.py
+++ utils/getdata.py
@@ -1,12 +1,16 @@
 import json
 
 from utils.path import file_path
+from utils.print import print_error
 
 
 def get_json_data(name: str, directory: str) -> object:
-    file = file_path(name, directory) + ".json"
+	try:
+		file = file_path(name, directory) + ".json"
 
-    with open(file, "r", encoding="utf-8") as json_file:
-        data: object = json.load(json_file)
+		with open(file, "r", encoding="utf-8") as json_file:
+			data: object = json.load(json_file)
 
-    return data
+		return data
+	except Exception as e:
+		print_error(f"Error to load {name}.json: ", e)
